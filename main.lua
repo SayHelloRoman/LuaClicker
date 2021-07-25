d98e31304f5bd33186993662fc39995d828d47a0
Roman <68429513+SayHelloRoman@users.noreply.github.com>
2021-07-25 18:39:44 +0300

    android

--- main.lua
+++ main.lua
@@ -5,27 +5,18 @@ local background = display.newImageRect( "background.png", 360, 570 )
 background.x = display.contentCenterX
 background.y = display.contentCenterY
 
-local function handleButtonEvent( event )
- 
-    if ( "ended" == event.phase ) then
-        button1:setLabel(button1:getLabel() + 1)
-    end
+local function touch( event )
+    Label.text = Label.text + 1
 end
 
-button1 = widget.newButton(
-    {
-        left = 100,
-        top = 200,
-        id = "button1",
-        label = 1,
-        onEvent = handleButtonEvent,
-        fontSize = 150,
-        labelColor = 
-        {
-            default={ 0, 0, 0 }
-        }
-    }
+Runtime:addEventListener( "tap", touch )
+
+Label = display.newText(
+    0,
+    display.contentCenterX,
+    display.contentCenterY,
+    native.systemFont,
+    150
 )
 
-button1.x = display.contentCenterX
-button1.y = display.contentCenterY
+Label:setFillColor( 0, 0, 0 )
